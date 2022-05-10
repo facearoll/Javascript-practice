19abcf32db0ba73b42cc710d81bdbf51452ada96
Jniko <nikolaisenmail@gmail.com>
2022-05-10 14:23:50 +0200

    Added onclick and uploaded img

---
+++ Div_øving/art.js
@@ -0,0 +1,16 @@
+
+let button = document.querySelector("button-74"); 
+
+function openWindow(){
+    window.open("https://www.google.com/maps?q=99+king+street,+newport,+usa&sxsrf=ALiCzsZCKCZDiU5byGYgEHsiHdzOLTh0_g:1652183073581&gs_lcp=Cgdnd3Mtd2l6EAEYAzIHCCMQ6gIQJzIHCCMQ6gIQJzIHCCMQ6gIQJzIHCCMQ6gIQJzIHCCMQ6gIQJzIHCCMQ6gIQJzIHCCMQ6gIQJzIHCCMQ6gIQJzIHCCMQ6gIQJzIHCCMQ6gIQJ0oECEEYAEoECEYYAFAAWABgixBoAXABeACAAQCIAQCSAQCYAQCgAQGwAQrAAQE&um=1&ie=UTF-8&sa=X&ved=2ahUKEwj48u7L7dT3AhXFSfEDHdU_AVkQ_AUoAXoECAEQAw" 
+    );
+}
+function openFacebook(){
+    window.open("https://www.facebook.com")
+}
+function openTwitter(){
+    window.open("https://www.Twitter.com")
+}
+function openPinterset(){
+    window.open("https://www.babyboomers.com/")
+}
